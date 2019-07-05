82144b345b5de51ac53dd6c33334ce4f498c0c8c
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-07-05 17:11:09 +0800

    add constants in wasm spec

---
+++ src/common/spec/magic.h
@@ -0,0 +1,12 @@
+//
+// Created by 31838 on 7/5/2019.
+//
+
+#ifndef UWE_MAGIC_H
+#define UWE_MAGIC_H
+
+#include <common/type.h>
+
+byte magic[4] = {0x00, 0x61, 0x73, 0x6d}; // "\0asm"
+
+#endif //UWE_MAGIC_H
---
+++ src/common/spec/spec.c
@@ -0,0 +1,5 @@
+//
+// Created by 31838 on 7/5/2019.
+//
+
+//placeholder
