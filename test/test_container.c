45f37d9f0870b0d1bb13c164adb79957958fdc59
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-07-05 11:55:39 +0800

    add test for array

--- test/test_container.c
+++ test/test_container.c
@@ -8,54 +8,69 @@ int comp(const void *a, const void *b) {
 void test_array() {
     array arr = array_init(10, sizeof(int));
 
-    TEST_ASSERT_EQUAL_INT(10, array_size(arr));
+    {
+        TEST_ASSERT_EQUAL_INT(10, array_size(arr));
+        //test getter/setter
+        for (int i = 0; i < 10; i++)
+            TEST_ASSERT_EQUAL_INT(0, array_set(arr, i, &i));
 
-    for (int i = 0; i < 10; i++)
-        TEST_ASSERT_EQUAL_INT(0, array_set(arr, i, &i));
-    for (int i = 0; i < 10; i++) {
-        int p;
-        TEST_ASSERT_EQUAL_INT(0, array_get(&p, arr, i));
-        TEST_ASSERT_EQUAL_INT(i, p);
+        for (int i = 0; i < 10; i++) {
+            int p;
+            TEST_ASSERT_EQUAL_INT(0, array_get(&p, arr, i));
+            TEST_ASSERT_EQUAL_INT(i, p);
+        }
     }
 
-    array copy = array_copy(arr);
-    TEST_ASSERT_EQUAL_INT(10, array_size(copy));
+    {//copy arr
+        array copy = array_copy(arr);
+        //copy should be as same as arr
+        TEST_ASSERT_EQUAL_INT(10, array_size(copy));
 
-    for (int i = 0; i < 10; i++)
-        TEST_ASSERT_EQUAL_INT(0, array_set(copy, i, &i));
-    for (int i = 0; i < 10; i++) {
-        int p;
-        TEST_ASSERT_EQUAL_INT(0, array_get(&p, copy, i));
-        TEST_ASSERT_EQUAL_INT(i, p);
-    }
-    array_destroy(copy);
+        for (int i = 0; i < 10; i++)
+            TEST_ASSERT_EQUAL_INT(0, array_set(copy, i, &i));
 
-    int *ptr = (int *) malloc(sizeof(int) * 10);
-    array_copy_to_array(ptr, arr);
-    for (int i = 0; i < 10; i++)
-        TEST_ASSERT_EQUAL(i, ptr[i]);
-    free(ptr);
+        for (int i = 0; i < 10; i++) {
+            int p;
+            TEST_ASSERT_EQUAL_INT(0, array_get(&p, copy, i));
+            TEST_ASSERT_EQUAL_INT(i, p);
+        }
 
-    ptr = (int *) array_get_data(arr);
-    for (int i = 0; i < 10; i++)
-        TEST_ASSERT_EQUAL(i, ptr[i]);
-
-    //slice arr[2,4)
-    array slice = array_slice(arr, 2, 4);
-    TEST_ASSERT_EQUAL(2, array_size(slice));
-    for (int i = 2; i < 4; i++) {
-        int p;
-        TEST_ASSERT_EQUAL(0, array_get(&p, slice, i - 2));
-        TEST_ASSERT_EQUAL(i, p);
+        TEST_ASSERT_EQUAL(NULL, array_destroy(copy));
     }
 
-    array append = array_append(arr, slice);
-    ptr = (int *) array_get_data(append);
-    int expected[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3};
-    TEST_ASSERT_EQUAL_INT_ARRAY(expected, ptr, 12);
+    {
+        int *ptr = (int *) malloc(sizeof(int) * 10);
+        array_copy_to_array(ptr, arr);
+        for (int i = 0; i < 10; i++)
+            TEST_ASSERT_EQUAL(i, ptr[i]);
+        free(ptr);
+    }
 
-    TEST_ASSERT_EQUAL(NULL, array_destroy(append));
-    TEST_ASSERT_EQUAL(NULL, array_destroy(slice));
+    {
+        int *ptr = (int *) array_get_data(arr);
+        for (int i = 0; i < 10; i++)
+            TEST_ASSERT_EQUAL(i, ptr[i]);
+    }
+
+    {//slice arr[2,4)
+        array slice = array_slice(arr, 2, 4);
+        TEST_ASSERT_EQUAL(2, array_size(slice));
+        for (int i = 2; i < 4; i++) {
+            int p;
+            TEST_ASSERT_EQUAL(0, array_get(&p, slice, i - 2));
+            TEST_ASSERT_EQUAL(i, p);
+        }
+
+        {
+            array append = array_append(arr, slice);
+            TEST_ASSERT_EQUAL_INT(12, array_size(append));
+            int *ptr = (int *) array_get_data(append);
+            int expected[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 2, 3};
+            TEST_ASSERT_EQUAL_INT_ARRAY(expected, ptr, 12);
+            TEST_ASSERT_EQUAL(NULL, array_destroy(append));
+        }
+        TEST_ASSERT_EQUAL(NULL, array_destroy(slice));
+    }
     TEST_ASSERT_EQUAL(NULL, array_destroy(arr));
 }
 
