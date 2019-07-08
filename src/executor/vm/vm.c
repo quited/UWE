b2a1d064e32711166a29b1b68e5578c90cd4fe3e
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-07-08 14:07:44 +0800

    initial implementation of vm

---
+++ src/executor/vm/vm.c
@@ -0,0 +1,14 @@
+//
+// Created by 31838 on 7/8/2019.
+//
+
+#include <common/type.h>
+#include "vm.h"
+
+struct internal_vm {
+  int placeholder;
+};
+
+vm vm_init() {
+  return NULL;
+}
---
+++ src/executor/vm/vm.h
@@ -0,0 +1,12 @@
+//
+// Created by 31838 on 7/8/2019.
+//
+
+#ifndef UWE_VM_H
+#define UWE_VM_H
+
+typedef struct internal_vm *vm;
+
+vm vm_init();
+
+#endif //UWE_VM_H
