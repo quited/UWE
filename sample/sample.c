dea419ca0c6beab49419b44343324137303fe27a
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-07-15 14:27:07 +0800

    complete sample;add sample.wasm

--- sample/sample.c
+++ sample/sample.c
@@ -1,4 +1,10 @@
-/*emcc sample.c -s "EXPORTED_FUNCTIONS=['_add','call_import_from_outside_environment']" -s ERROR_ON_UNDEFINED_SYMBOLS=0*/
+/*
+ emcc sample.c \
+ -s "EXPORTED_FUNCTIONS=['_add','call_import_from_outside_environment']" \
+ -s ERROR_ON_UNDEFINED_SYMBOLS=0 \
+ -O3 \
+ -o sample.wasm
+*/
 
 int add(int a, int b) {
   return a + b;
